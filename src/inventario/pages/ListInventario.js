1167b756e45c2a1c88328a00d3f959b5bcaf0119
sdavdf <davidfueltala90@hotmail.com>
2024-05-28 10:04:23 -0500

    Generar pdf

--- src/inventario/pages/ListInventario.js
+++ src/inventario/pages/ListInventario.js
@@ -1,221 +1,211 @@
-import React from 'react'
-import DashboardInventario from './components/DashboardInventario'
-import { useGetInventarioTodoQuery } from '../services/inventarioApi'
-import MUIDataTable from 'mui-datatables';
-import ModalInventario from './components/ModalInventario'
-
+import React, { useEffect, useState } from "react";
+import DashboardInventario from "./components/DashboardInventario";
+import { useGetInventarioTodoQuery } from "../services/inventarioApi";
+import MUIDataTable from "mui-datatables";
+import ModalInventario from "./components/ModalInventario";
+import reporte_inventario_banner from "../../assets/reporte_inventario_banner.png";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 
 export default function ListInventario() {
-    const user = JSON.parse(localStorage.getItem('user') || "{}")
-    const { data, isSuccess, isLoading, isError, error } = useGetInventarioTodoQuery(user.access)
-    const userDatos = JSON.parse(localStorage.getItem('userDatos') || "{}")
+  const user = JSON.parse(localStorage.getItem("user") || "{}");
+  const userDatos = JSON.parse(localStorage.getItem("userDatos") || "{}");
 
+  const { data, isSuccess, isLoading, isError, error } =
+    useGetInventarioTodoQuery(user.access);
 
+  const [filteredData, setFilteredData] = useState([]);
 
+  useEffect(() => {
+    if (isSuccess) {
+      setFilteredData(data);
+    }
+  }, [isSuccess, data]);
 
-    const columns = [
-        {
-            name: 'cod_unico',
-            label: 'cod Unico',
-            options: {
-                customBodyRender: (value) => {
-                    return (
-                        <div >
-                            <p className='text-xs text-center'>{value}</p>
-                        </div>
-                    )
-                }
-            }
+  const columns = [
+    {
+      name: "cod_unico",
+      label: "Código Único",
+    },
+    {
+      name: "cod_senescyt",
+      label: "Código Senescyt",
+    },
+    {
+      name: "cod_instituto",
+      label: "Código Instituto",
+    },
+    {
+      name: "descripcion",
+      label: "Descripción",
+    },
+    {
+      name: "modelo",
+      label: "Modelo",
+    },
+    {
+      name: "serie",
+      label: "Serie",
+    },
+    {
+      name: "color",
+      label: "Color",
+    },
+    {
+      name: "marca",
+      label: "Marca",
+    },
+    {
+      name: "asignado_name",
+      label: "Asignado",
+    },
+    {
+      name: "ubicacion_name",
+      label: "Ubicación",
+    },
+    {
+      name: "digitador_name",
+      label: "Digitador",
+    },
+    {
+      name: "",
+      label: "",
+      options: {
+        customBodyRender: (value, tableMeta) => {
+          return (
+            <>
+              {userDatos.is_adminInventario && userDatos.is_docente ? (
+                <ModalInventario id={parseInt(tableMeta.rowData[12])} />
+              ) : (
+                <> </>
+              )}
+            </>
+          );
         },
-        {
-            name: 'cod_senescyt',
-            label: 'cod Senescyt',
-            options: {
-                customBodyRender: (value) => {
-                    return (
-                        <div >
-                            <p className='text-xs text-center'>{value}</p>
-                        </div>
-                    )
-                }
-            }
-        },
-        {
-            name: 'cod_instituto',
-            label: 'cod ISTVIF',
-            options: {
-                customBodyRender: (value) => {
-                    return (
-                        <div >
-                            <p className='text-xs text-center'>{value}</p>
-                        </div>
-                    )
-                }
-            }
-        },
-        {
-            name: 'descripcion',
-            label: 'descripcion',
-            options: {
-                customBodyRender: (value) => {
-                    return (
-                        <div >
-                            <p className='text-xs text-center'>{value}</p>
-                        </div>
-                    )
-                }
-            }
+      },
+    },
+    {
+      name: "id",
+      label: "",
+      options: {
+        display: false,
+      },
+    },
+  ];
 
-        },
-        {
-            name: 'modelo',
-            label: 'modelo',
-            options: {
-                customBodyRender: (value) => {
-                    return (
-                        <div >
-                            <p className='text-xs text-center'>{value}</p>
-                        </div>
-                    )
-                }
-            }
-        },
-        {
-            name: 'serie',
-            label: 'serie',
-            options: {
-                customBodyRender: (value) => {
-                    return (
-                        <div >
-                            <p className='text-xs text-center'>{value}</p>
-                        </div>
-                    )
-                }
-            }
-        },
-        {
-            name: 'color',
-            label: 'color',
-            options: {
-                customBodyRender: (value) => {
-                    return (
-                        <div >
-                            <p className='text-xs text-center'>{value}</p>
-                        </div>
-                    )
-                }
-            }
-        },
-        {
-            name: 'marca',
-            label: 'marca',
-            options: {
-                customBodyRender: (value) => {
-                    return (
-                        <div >
-                            <p className='text-xs text-center'>{value}</p>
-                        </div>
-                    )
-                }
-            }
-        },
+  const options = {
+    selectableRows: "none",
+    onTableChange: (action, tableState) => {
+      if (action === "filterChange") {
+        const filteredData = tableState.displayData.map((item) => item.data);
+        setFilteredData(filteredData);
+      }
+    },
+    responsive: "horizontal",
+  };
 
-        {
-            name: 'asignado_name',
-            label: 'Asignado',
-            options: {
-                customBodyRender: (value) => {
-                    return (
-                        <div >
-                            <p className='text-xs text-center'>{value}</p>
-                        </div>
-                    )
-                }
-            }
-        },
-        {
-            name: 'ubicacion_name',
-            label: 'ubicacion',
-            options: {
-                customBodyRender: (value) => {
-                    return (
-                        <div >
-                            <p className='text-xs text-center'>{value}</p>
-                        </div>
-                    )
-                }
-            }
-        },
-        {
-            name: 'digitador_name',
-            label: 'digitador',
-            options: {
-                display: false,
-            }
-        },
-        {
-            name: '',
-            label: '',
-            options: {
-                customBodyRender: (value, tableMeta) => {
+  const exportPDF = () => {
+    let dataToExport = filteredData; 
 
-                    return (
-                        <>
-                            {userDatos.is_adminInventario && userDatos.is_docente ?
-                                <ModalInventario id={parseInt(tableMeta.rowData[12])} />
+    if (!filteredData || filteredData.length === 0) {
+      dataToExport = data;
+    }
 
-                                :
-                                <> </>
-                            }
-                        </>
+    if (!dataToExport || dataToExport.length === 0) {
+      alert("No hay datos para exportar.");
+      return;
+    }
 
-                    )
-                }
-            }
-        },
-        {
-            name: 'id',
-            label: '',
-            options: {
-                display: false,
-            }
-        },
-    ]
-    const options = {
-        selectableRows: 'none', // Deshabilita la selección en la primera fila
+    const unit = "pt";
+    const size = "A4";
+    const orientation = "portrait";
 
-        titleTextStyle: {
-            fontSize: '5px', // Establece el tamaño de fuente del título
-        },
-        responsive: 'horizontal'
+    const doc = new jsPDF(orientation, unit, size);
+
+    doc.setFontSize(14);
+
+    const headers = [
+      [
+        "Código Único",
+        "Código Senescyt",
+        "Código Instituto",
+        "Descripción",
+        "Modelo",
+        "Serie",
+        "Color",
+        "Marca",
+        "Asignado",
+        "Ubicación",
+        "Digitador",
+      ],
+    ];
+
+    const content = {
+      startY: 150,
+      head: headers,
+      body: dataToExport.map((elt) => [
+        elt.cod_unico,
+        elt.cod_senescyt,
+        elt.cod_instituto,
+        elt.descripcion,
+        elt.modelo,
+        elt.serie,
+        elt.color,
+        elt.marca,
+        elt.asignado_name,
+        elt.ubicacion_name,
+        elt.digitador_name,
+      ]),
+      styles: { fontSize: 6 },
     };
 
-    return (
-        <DashboardInventario>
-            {userDatos.is_adminInventario ?
-                <button className='bg-blue-900 rounded text-white mb-2'>
-                    <a href='/inventario/register'> Nuevo registro</a>  </button>
-                :
-                <> </>
-            }
+    const img = new Image();
+    img.src = reporte_inventario_banner;
+    img.onload = function () {
+      doc.addImage(this, "PNG", 10, 10, 578, 90);
+      doc.autoTable(content);
 
+      const pageCount = doc.internal.getNumberOfPages();
+      const footerText = `Generado por: ${userDatos.first_name} ${userDatos.last_name}`;
+      for (let i = 1; i <= pageCount; i++) {
+        doc.setPage(i);
+        doc.setFontSize(7);
+        doc.text(
+          footerText,
+          doc.internal.pageSize.getWidth() / 2,
+          doc.internal.pageSize.getHeight() - 30,
+          { align: "center" }
+        );
+      }
 
-            <MUIDataTable
-                title={'Lista de assets'}
-                data={data}
-                columns={columns}
-                options={options}
-            />
+      doc.save("Inventario.pdf");
+    };
+  };
 
+  return (
+    <DashboardInventario>
+      <button
+        onClick={exportPDF}
+        className="mb-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Generar PDF
+      </button>
+      {userDatos.is_adminInventario ? (
+        <button className="bg-blue-900 rounded text-white mb-2">
+          <a href="/inventario/register"> Nuevo registro</a>{" "}
+        </button>
+      ) : (
+        <> </>
+      )}
 
-
-
-
-        </DashboardInventario>
-
-    )
+      <div className="py-8">
+        <MUIDataTable
+          title={"all"}
+          data={filteredData}
+          columns={columns}
+          options={options}
+        />
+      </div>
+    </DashboardInventario>
+  );
 }
-
-
-
-
