13826c534523b46fe79098369c52c700fbee1b37
Diego <diegoromo_408@hotmail.com>
2024-10-18 09:13:30 -0500

    spinner de carga en modulo evaluacion

--- src/evaluacion/pages/EvaluacionEvidencia.js
+++ src/evaluacion/pages/EvaluacionEvidencia.js
@@ -3,17 +3,18 @@ import Navbar_dashboard from '../../pages/components/Navbar_dashboard'
 import { useGetEvaluacionEvidenciaQuery, useGetCriteriosQuery, useGetEvaluacionEvidencia_modeloCriterioQuery } from '../services/evaluacionApi'
 import CriterioButton from '../components/CriterioButton'
 import CriterioDataTable from '../components/CriterioDataTable'
+import LoadingSpinner from '../components/LoadingSpinner'
 
 
 export default function EvaluacionEvidencia() {
   const user = JSON.parse(localStorage.getItem('user') || "{}")
   const userDatos = JSON.parse(localStorage.getItem('userDatos') || "{}")
   const { data, isSuccess } = useGetEvaluacionEvidenciaQuery(user.access)
-  const { data: dataCriterios, isSuccess: isSuccessCriterios } = useGetCriteriosQuery(user.access)
+  const { data: dataCriterios, isSuccess: isSuccessCriterios, isLoading:isLoadingCriterios,isFetching:isFetchingCriterios } = useGetCriteriosQuery(user.access)
 
   const [selectedCriterioId, setSelectedCriterioId] = useState(null);
 
-  const { data: dataEvaluacion, isLoading: isLoadingEvaluacion, isSuccess:isSuccessEvaluacion } = useGetEvaluacionEvidencia_modeloCriterioQuery({ access: user.access, criterio_id: selectedCriterioId, modelo_id: 1 }, {
+  const { data: dataEvaluacion, isLoading: isLoadingEvaluacion, isSuccess:isSuccessEvaluacion, isFetching:isFetchingEvaluacion } = useGetEvaluacionEvidencia_modeloCriterioQuery({ access: user.access, criterio_id: selectedCriterioId, modelo_id: 1 }, {
     skip: selectedCriterioId === null,
   });
 
@@ -30,7 +31,9 @@ export default function EvaluacionEvidencia() {
       <Navbar_dashboard />
       <div className='text-xs text-gray-500 mb-1'>Evidencia de acuerdo al modelo de evaluación</div>
       <div>
-        {isSuccessCriterios ?
+        {(isFetchingCriterios || isLoadingCriterios) ?
+        <LoadingSpinner/>
+        :
           <div>
             {dataCriterios.map((item) => (
               <CriterioButton key={item.id} criterio={item} onClick={handleCriterioClick} />
@@ -40,8 +43,7 @@ export default function EvaluacionEvidencia() {
              <a href='/evaluacion/estadistica' className='text-xs'>Estadistica</a>
              {userDatos.is_rectora && <a href='/evaluacion/documentos/docente' className='ml-4 text-xs'>Por docente</a>}
           </div>
-          :
-          <>Cargando criterios</>
+  
         }
        
       </div>
@@ -49,13 +51,13 @@ export default function EvaluacionEvidencia() {
       <div>
         {selectedCriterioId && (
           <div>
-            {isSuccessEvaluacion ?
-              
+            {(isLoadingEvaluacion || isFetchingEvaluacion) ?
+            <LoadingSpinner/>
+            :
               <CriterioDataTable
               dataCriterios={dataEvaluacion}
             />
-              :
-              <p>cargando datos</p>
+    
             }
           </div>
 
